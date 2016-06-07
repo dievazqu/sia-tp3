b15f2cbd4f40b072e8523dea077f0220826de169
Diego <dievazqu@itba.edu.ar>
2016-06-07 00:03:49 -0300

    Test more generic

--- src/test/Test.java
+++ src/test/Test.java
@@ -10,13 +10,14 @@ import config.Param;
 
 public class Test {
 
-	private static final int generationSize = 6;
+	private static final int alleleSize = 10;
+	private static final int generationSize = 10;
 	private static final int maxEpochs = 100;
 	
 	public static void main(String[] args) {
 		List<Individual> list = new ArrayList<Individual>();
 		for(int i=0; i<generationSize; i++){
-			list.add(new Individual(TestBitAllele.initialBitAlleles(5), TestBitAllele::fitness));
+			list.add(new Individual(TestBitAllele.initialBitAlleles(alleleSize), TestBitAllele::fitness));
 		}
 		Population pop = new Population(new Param(null), list);
 		for(int i=0; i<maxEpochs; i++){
